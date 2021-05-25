bf4db3e0608e6c8d82f013d8b923dc5c43f88b1c
Czestochowska Justyna Anna <czestoch@iccluster111.iccluster.epfl.ch>
2021-05-25 17:48:06 +0200

    contextual analysis for non in tokenizer emojis

--- settings.py
+++ settings.py
@@ -15,6 +15,7 @@ CLDR_ANNS_PATH = os.path.join(project_dir, "data", "external", "cldr_39_alpha4_e
 CLDR_DISTANCE = os.path.join(project_dir, "data", "interim", "cldr_distance.csv.gz")
 AMBIGUITY_VARIATION = os.path.join(project_dir, "data", "interim", "ambiguity_variation.csv.gz")
 EMOJI_IMGS = os.path.join(project_dir, "data", "external", "emoji_imgs")
+EMOJI_IMGS_CLUSTER = os.environ.get("EMOJI_IMGS_CLUSTER")
 EMOJI_CATEGORIZED = os.path.join(project_dir, "data", "external", "emoji_categories.pkl")
 E2V_EMBEDDINGS = os.path.join(project_dir, "data", "raw", "embeddings", "word2vec", "em_dataset", "emoji2vec.bin")
 E2V_MAPPING = os.path.join(project_dir, "data", "raw", "embeddings", "word2vec", "em_dataset", "mapping.pk")
